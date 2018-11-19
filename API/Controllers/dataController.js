3bc579501734861b44af6b771b1b9fa7472effa7
RaeanneVillanueva <45121475+RaeanneVillanueva@users.noreply.github.com>
2018-11-19 16:17:03 +0800

    Update dataController.js

--- API/Controllers/dataController.js
+++ API/Controllers/dataController.js
@@ -25,8 +25,8 @@ function getDogDescription(req, res) {
                /* speech: 'Something went wrong!',
                 displayText: 'Something went wrong!',
                 source: 'dogDescription'*/
-                fulfillmentText: "Something went wrong!",
-                source: "dogDescription"
+                fulfillmentMesssages:["text":{text:["Something went wrong!"]}]
+                
             });
         }
 
@@ -35,16 +35,14 @@ function getDogDescription(req, res) {
                 /*speech: breedExists.description,
                 displayText: breedExists.description,
                 source: 'dogDescription'*/
-                 fulfillmentText: breedExists.description,
-                 source: "dogDescription"
+                 fulfillmentMesssages:["text":{text:[breedExists.description]}]
             });
         } else {
             return res.json({
                /*speech: 'I currently have no information about this breed',
                displayText: 'I currently have information about this breed',
                source: 'dogDescription'*/
-               fulfillmentText: "I currently have no information about this breed",
-                source: "dogDescription"
+               fulfillmentMesssages:["text":{text:["I have no information about this breed"]}]
             });
         }
     });
